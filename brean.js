4538432ac3b9680ce68e87d13047ccb752737bff
sachin jaiswal <sj62513@gmail.com>
2022-02-04 16:08:38 +0530

    update

--- brean.js
+++ brean.js
@@ -1,16 +1,45 @@
-let text = document.getElementById("typing_quote").innerText
+let text = document.getElementById("typing_quote")
 let inputPlace = document.querySelector("#typing_input")
-inputPlace.addEventListener("input",function(){
-    let inputText = inputPlace.value
-   for(let i = 0;i<inputText.length;i++){
-     
-       if(text[i]!=inputText[i]){
-          alert("wrong typed")
-       }
-   }
-})
 let timer = document.querySelector("#typing_timer")
+turningSpan()
+function turningSpan(){
+    const arrOfText = text.innerText.split("")
+    arrOfText.forEach((e,index)=>{
+        const elem = document.createElement("span")
+        elem.innerText = e
+        arrOfText[index] = elem
+    })
+    text.innerHTML = ""
+   
+    arrOfText.forEach((e)=>{
+     text.appendChild(e)
+    })
+   
+}
+inputPlace.addEventListener("input",()=>{
+    let arrOfSpan = text.querySelectorAll("span")
+    let arrOfValue = inputPlace.value.split("")
+    arrOfSpan.forEach((e,index)=>{
+        if(arrOfValue[index] === e.innerText){
+            arrOfSpan[index].classList.add("correct")
+            arrOfSpan[index].classList.remove("incorrect")
+        }
+        else if(index<arrOfValue.length && arrOfValue[index]!== e.innerText){
+            arrOfSpan[index].classList.remove("correct")
+            arrOfSpan[index].classList.add("incorrect")
+        }
+        else{
+            arrOfSpan[index].classList.remove("correct")
+            arrOfSpan[index].classList.remove("incorrect")
+        }
+        
+     
+       
+    })
+})
+
 setInterval(()=>{
     let num = Number(timer.innerText)
     timer.innerHTML = num+1
 },1000)
+
